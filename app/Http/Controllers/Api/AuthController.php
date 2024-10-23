c539fd04bab3a91db6cfb864dc8f3bb71f5b808d
EngyMohamed5 <engymohamedalratmy@gmail.com>
2024-10-23 18:28:27 +0300

    logout function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -44,6 +44,14 @@ class AuthController extends Controller
 
 
 
+    public function logout(Request $request)
+    {
+        // Revoke the token that was used to authenticate the current request
+        $request->user()->currentAccessToken()->delete();
 
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
