cbef1dc9c82d812e2ace2eefa98d3d80d30eac01
anton <antonrizk71@gmail.com>
2024-10-23 12:23:37 +0300

    update api file

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// wqDMEFEMMM,MDFS
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
