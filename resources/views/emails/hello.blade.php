fcad8b8fe3df1f1cc105a032f0d46051bb10043c
anton <antonrizk71@gmail.com>
2024-10-26 11:27:34 +0300

    add logo to hello mail

--- resources/views/emails/hello.blade.php
+++ resources/views/emails/hello.blade.php
@@ -4,28 +4,20 @@
 <head>
     <title>Hello Mail</title>
 </head>
-<style>
-    .container {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-top: 5em;
-    }
-</style>
 
-<body>
-<div class="container">
-    <div>
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 20px;">
+<div style="display: flex; justify-content: space-between; align-items: center; margin-top: 5em;">
+    <div style="max-width: 50%;">
         <h1>Hello {{$name}}!</h1>
-        <p>Welcome To Benha University</p>
+        <p>Welcome to Benha University</p>
         <p>Thank you for joining us!</p>
         <span>Regards,</span>
         <h4>{{ config('app.name') }}</h4>
     </div>
-
-    <img src="{{ asset('assets/Benha_University_Logo.png') }}" alt="Benha University Logo">
+    <div style="max-width: 30%;">
+        <img src="{{ $message->embed(public_path().'/assets/Benha_University_Logo.png') }}" alt="Benha University Logo" style="max-width: 100%; height: auto;">
+    </div>
 </div>
-
 </body>
 
 </html>
